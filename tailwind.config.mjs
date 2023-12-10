ece0d7d6559b09a92be528c15b5eafb7fb8aa280
{syou} <{itokaizi810@gmail.com}>
2023-12-10 23:47:27 +0900

    fix:tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage:{
+				'home-image':"url(public/img/26643688_m.jpg)"
+			},
+			fontFamily:{
+				sans: ["DotGothic16", ...defaultTheme.fontFamily.sans],
+
+			}
+		},
 	},
 	plugins: [],
 }
